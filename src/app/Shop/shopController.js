7a311afc6bd7313e40db23aa368625a7883256dc
chldppwls12 <chldppwls12@naver.com>
2021-11-20 03:13:33 +0900

    🎨 상품 상세 조회 API 쪽 jwt 검증 추가

--- src/app/Shop/shopController.js
+++ src/app/Shop/shopController.js
@@ -3,12 +3,26 @@ const shopService = require("./shopService");
 const baseResponse = require("../../../config/baseResponseStatus");
 const {response} = require("../../../config/response");
 const {errResponse} = require("../../../config/response");
+const jwt = require("jsonwebtoken");
 
+require('dotenv').config();
 
 exports.getProductDetail = async (req, res) => {
-  const {productIdx} = req.params;
+  const token = req.headers['x-access-token'];
+  let params = [token];
 
-  let params = [productIdx];
+  //jwt가 있을 경우 유효한지 확인
+  let userIdx;
+  if (token){
+    try{
+      userIdx = jwt.verify(token, process.env.jwtSecret);
+    }catch(err){
+      return res.send(errResponse(baseResponse.TOKEN_VERIFICATION_FAILURE));
+    }
+  }
+
+  const {productIdx} = req.params;
+  params = [productIdx];
   
   //상품 detail
   const productDetail = await shopProvider.getProductDetail(params);
