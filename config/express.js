60d0e3eff7f50589b741cf16dec21ff592b21b5a
chldppwls12 <chldppwls12@naver.com>
2021-11-23 20:16:10 +0900

    🚧 세션 오류 수정 중

--- config/express.js
+++ config/express.js
@@ -3,6 +3,19 @@ const compression = require('compression');
 const methodOverride = require('method-override');
 let cors = require('cors');
 const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+
+require('dotenv').config();
+
+const options = {
+  host: process.env.dbHost,
+  user: 'admin',
+  port: '3306',
+  password: process.env.dbPassword,
+  database: process.env.dbName
+}
+
+const sessionStore = new MySQLStore(options);
 
 module.exports = function (){
   const app = express();
@@ -16,8 +29,9 @@ module.exports = function (){
     key: 'sid',
     secret: 'secret',
     resave: false,
-    saveUninitialized: false,
-    cookie: {maxAge: 1000 * 60 * 3}
+    saveUninitialized: true,
+    cookie: {maxAge: 1000 * 60 * 3},
+    store: sessionStore
   }));
 
   require('../src/app/User/userRoute')(app);
