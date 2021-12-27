5e09648f94ce003b132ab6fe3301388747582065
chldppwls12 <chldppwls12@naver.com>
2021-12-27 16:27:02 +0900

    🐛 require 오류 수정

--- config/express.js
+++ config/express.js
@@ -47,7 +47,7 @@ module.exports = function (){
   require('../src/app/Story/storyRoute')(app);
   require('../src/app/StoryComment/storyCommentRoute')(app);
   require('../src/app/QnA/qnaRoute')(app);
-  require('../src/app/ABTest/abtestRoute')(app);
+  require('../src/app/ABTest/abTestRoute')(app);
   require('../src/app/QnAComment/qnaCommentRoute')(app);
   require('../src/app/ABTestComment/abTestCommentRoute')(app);
   require('../src/app/MyPage/myPageRoute')(app);
