da932d0432fef6401352f77a29eecb7fd4e0c09b
chldppwls12 <chldppwls12@naver.com>
2021-12-30 02:46:13 +0900

    🎨 작가 공간 uri 변경

--- src/app/ArtistPlace/artistPlaceRoute.js
+++ src/app/ArtistPlace/artistPlaceRoute.js
@@ -6,14 +6,14 @@ module.exports = function (app){
   app.get('/artists/:artistIdx', artistPlaceController.getArtistInfo);
 
   //작가 별 craft 페이지 개수 조회 API
-  app.get('/artist/:artistIdx/crafts/page', artistPlaceController.getArtistCraftTotalPage);
+  app.get('/artists/:artistIdx/crafts/page', artistPlaceController.getArtistCraftTotalPage);
 
   //작가 별 craft 조회 API
-  app.get('/artist/:artistIdx/crafts', artistPlaceController.getArtistCraft);
+  app.get('/artists/:artistIdx/crafts', artistPlaceController.getArtistCraft);
 
   //작가 별 article 페이지 개수 조회 API
-  app.get('/artist/:artistIdx/articles/page', artistPlaceController.getArtistArticleTotalPage);
+  app.get('/artists/:artistIdx/articles/page', artistPlaceController.getArtistArticleTotalPage);
 
   //작가 별 article 조회 API
-  app.get('/artist/:artistIdx/articles', artistPlaceController.getArtistArticle);
+  app.get('/artists/:artistIdx/articles', artistPlaceController.getArtistArticle);
 }
