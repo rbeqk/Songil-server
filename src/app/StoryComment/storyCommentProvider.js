f496a6ed785eb4ffe2c8df5c86ba1ee20332203f
chldppwls12 <chldppwls12@naver.com>
2022-01-11 02:36:32 +0900

    🔥 Story 댓글 불필요한 코드 삭제

--- src/app/StoryComment/storyCommentProvider.js
+++ src/app/StoryComment/storyCommentProvider.js
@@ -6,41 +6,6 @@ const {response, errResponse} = require('../../../config/response');
 const baseResponse = require('../../../config/baseResponseStatus');
 const {STORY_COMMENT_PER_PAGE} = require("../../../modules/constants");
 
-exports.getStoryCommentTotalPage = async (storyIdx) => {
-  try{
-    const connection = await pool.getConnection(async conn => conn);
-    try{
-
-      //존재하는 storyIdx인지
-      const isExistStoryIdx = await storyDao.isExistStoryIdx(connection, storyIdx);
-      if (!isExistStoryIdx){
-        connection.release();
-        return errResponse(baseResponse.INVALID_STORY_IDX);
-      }
-
-      const storyParentCommentCnt = await storyCommentDao.getStoryParentCommentCnt(connection, storyIdx);  //총 story Parent댓글 개수
-      const storyReCommentCnt = await storyCommentDao.getStoryReCommentCnt(connection, storyIdx);  //총 story Re댓글 개수
-      const storyCommentCnt = storyParentCommentCnt + storyReCommentCnt;
-
-      const pageItemCnt = 5;  //한 페이지당 보여줄 아이템 개수
-      const totalPages = (storyCommentCnt % pageItemCnt == 0) ? storyCommentCnt / pageItemCnt : parseInt(storyCommentCnt / pageItemCnt) + 1;  //총 페이지 수
-
-      const result = {'totalPages': totalPages};
-
-      connection.release();
-      return response(baseResponse.SUCCESS, result);
-      
-    }catch(err){
-      connection.release();
-      logger.error(`getStoryCommentTotalPage DB Query Error: ${err}`);
-      return errResponse(baseResponse.DB_ERROR);
-    }
-  }catch(err){
-    logger.error(`getStoryCommentTotalPage DB Connection Error: ${err}`);
-    return errResponse(baseResponse.DB_ERROR);
-  }
-}
-
 //스토리 댓글 조회
 exports.getStoryComment = async (storyIdx, userIdx, page) => {
   try{
