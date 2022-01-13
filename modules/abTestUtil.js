48389bc907b18442c2b9d345806aa2be714129b9
chldppwls12 <chldppwls12@naver.com>
2022-01-13 18:30:02 +0900

    ♻️ abTestInfo class 추가 및 사용

--- modules/abTestUtil.js
+++ modules/abTestUtil.js
@@ -1,6 +1,18 @@
 const abTestDao = require('../src/app/ABTest/abTestDao');
 const {logger} = require('../config/winston');
 
+class abTestInfo{
+  constructor(
+    vote,
+    totalVoteCnt,
+    percent
+  ) {
+    this.vote = vote;
+    this.totalVoteCnt = totalVoteCnt;
+    this.percent = percent
+  }
+}
+
 //투표 최종 결과 가져오기
 const getABTestFinalInfo = async (connection, abTestIdx) => {
   try{
@@ -13,35 +25,23 @@ const getABTestFinalInfo = async (connection, abTestIdx) => {
       
       //서로 비겼을 경우
       if (finalInfo[0]['totalCnt'] === finalInfo[1]['totalCnt']){
-        result = {
-          'vote': 'DRAW',
-          'totalVoteCnt': finalInfo[0]['totalCnt'],
-          'percent': 50
-        }
+        result = new abTestInfo('DRAW', finalInfo[0]['totalCnt'], 50);
       }
       else{
-        result = {
-          'vote': finalInfo[0]['vote'],
-          'totalVoteCnt': finalInfo[0]['totalCnt'],
-          'percent': parseInt(finalInfo[0]['totalCnt'] / (finalInfo[0]['totalCnt'] + finalInfo[1]['totalCnt']) * 100)
-        }
+        result = new abTestInfo(
+          finalInfo[0]['vote'],
+          finalInfo[0]['totalCnt'],
+          parseInt(finalInfo[0]['totalCnt'] / (finalInfo[0]['totalCnt'] + finalInfo[1]['totalCnt']) * 100)
+        );
       }
     }
     //몰표일 경우
     else if (finalInfo.length === 1){
-      result = {
-        'vote': finalInfo[0]['vote'],
-        'totalVoteCnt': finalInfo[0]['totalCnt'],
-        'percent': 100
-      }
+      result = new abTestInfo(finalInfo[0]['vote'], finalInfo[0]['totalCnt'], 100);
     }
     //아무도 투표하지 않았을 경우
     else if (finalInfo.length === 0){
-      result = {
-        'vote': 'DRAW',
-        'totalVoteCnt': 0,
-        'percent': 50
-      }
+      result = new abTestInfo('DRAW', 0, 50);
     }
 
   return result;
@@ -65,11 +65,7 @@ const getUserVoteInfo = async (connection, userIdx, abTestIdx) => {
       const totalVotedCnt = await abTestDao.getCurrentVoteTotalCnt(connection, abTestIdx);  //총 투표 수
       const userVotedTotalCnt = await abTestDao.getCurrentUserVoteTotalCnt(connection, abTestIdx, userVote['vote']);  //유저가 투표한 표의 총 투표 수
 
-      result = {
-        'vote': userVote['vote'],
-        'totalVoteCnt': userVotedTotalCnt,
-        'percent': parseInt(userVotedTotalCnt / totalVotedCnt * 100)
-      }
+      result = new abTestInfo(userVote['vote'], userVotedTotalCnt, parseInt(userVotedTotalCnt / totalVotedCnt * 100));
     }
     
     return result;
