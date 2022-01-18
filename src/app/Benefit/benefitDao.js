cd238de356b559d4d34d21423a4783e7a918de88
chldppwls12 <chldppwls12@naver.com>
2022-01-19 02:15:48 +0900

    🎨 보유 베네핏 조회 쿼리 변경

--- src/app/Benefit/benefitDao.js
+++ src/app/Benefit/benefitDao.js
@@ -3,15 +3,16 @@ async function getBenefits(connection, userIdx){
   SELECT UB.benefitIdx,
         B.imageUrl,
         IF(B.discountPrice IS NOT NULL,
-            CONCAT(B.basisPrice, '원 이상 구매 시 ', B.discountPrice, '원 할인'),
-            CONCAT(B.basisPrice, '원 이상 구매 시 ', B.discountPercent, '% 할인')) as discountInfo,
+            CONCAT(B.discountPrice, '원 할인'),
+            CONCAT(B.discountPercent, '% 할인'))                                 as discountInfo,
         CASE
             WHEN B.benefitCategoryIdx = 1 THEN B.title
-            WHEN B.benefitCategoryIdx = 2 THEN CONCAT(U.nickname, '작가 전용 베네핏')
+            WHEN B.benefitCategoryIdx = 2 THEN CONCAT(U.nickname, ' 작가 전용 베네핏')
             WHEN B.benefitCategoryIdx = 3 THEN NULL
-            END                                                           as title,
-        B.maxDiscountPrice,
-        DATE_FORMAT(B.deadline, '%Y.%m.%d')                               as deadline
+          END                                                               as title,
+        IF(B.discountPrice IS NOT NULL, NULL,
+            CONCAT(B.basisPrice, '원 이상 구매시, 최대 ', B.maxDiscountPrice, '원 할인')) as detailInfo,
+        DATE_FORMAT(B.deadline, '%m.%d')                                   as deadline
   FROM UserBenefit UB
           JOIN Benefit B ON B.benefitIdx = UB.benefitIdx && B.deadline > NOW() && B.isDeleted = 'N'
           LEFT JOIN Artist A ON A.artistIdx = B.artistIdx && A.isDeleted = 'N'
