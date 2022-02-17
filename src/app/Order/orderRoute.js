3f74de433caccc6a30fb21764129046bcff80262
chldppwls12 <chldppwls12@naver.com>
2022-02-17 19:22:15 +0900

    🎨 주문서 및 결제 관련 uri 변경

--- src/app/Order/orderRoute.js
+++ src/app/Order/orderRoute.js
@@ -3,17 +3,17 @@ module.exports  = function(app){
   const orderController = require('./orderController');
 
   //주문서 상품 추가 API
-  app.post('/order/crafts', jwtMiddleware, orderController.addCraftInOrderSheet);
+  app.post('/orders/crafts', jwtMiddleware, orderController.addCraftInOrderSheet);
 
   //베네핏 적용 API
-  app.post('/order/:orderIdx/benefits', jwtMiddleware, orderController.applyOrderBenefit);
+  app.post('/orders/:orderIdx/benefits', jwtMiddleware, orderController.applyOrderBenefit);
 
   //추가 배송비 적용 및 조회 API
-  app.post('/order/:orderIdx/extra-fee', jwtMiddleware, orderController.updateOrderExtraShippingFee);
+  app.post('/orders/:orderIdx/extra-fee', jwtMiddleware, orderController.updateOrderExtraShippingFee);
 
   //배송비 정보 및 사용 포인트 저장 API
-  app.post('/order/:orderIdx/etc-info', jwtMiddleware, orderController.updateOrderEtcInfo);
+  app.post('/orders/:orderIdx/etc-info', jwtMiddleware, orderController.updateOrderEtcInfo);
 
   //결제 검증 API
-  app.post('/order/:orderIdx', jwtMiddleware, orderController.validatePayment);
+  app.post('/orders/:orderIdx', jwtMiddleware, orderController.validatePayment);
 }
