48219e4f2fa22e83a9c867811a81737a02a0d0a5
chldppwls12 <chldppwls12@naver.com>
2022-02-17 19:25:47 +0900

    🎨 주문 시 적용 가능 베네핏 조회 uri 변경

--- src/app/Benefit/benefitRoute.js
+++ src/app/Benefit/benefitRoute.js
@@ -6,5 +6,5 @@ module.exports = function(app){
   app.get('/my-page/benefits', jwtMiddleware, benefitController.getBenefits);
 
   //주문 시 적용 가능 베네핏 조회 API
-  app.get('/order/:orderIdx/benefits', jwtMiddleware, benefitController.getCanUseBenefit);
+  app.get('/orders/:orderIdx/benefits', jwtMiddleware, benefitController.getCanUseBenefit);
 }
