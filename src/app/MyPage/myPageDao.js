f1d6796e2b1c02ed3334f02946e7ff08d6915a57
chldppwls12 <chldppwls12@naver.com>
2022-03-02 14:37:13 +0900

    🎨 댓글 단 글 조회 쪽 쿼리 수정

--- src/app/MyPage/myPageDao.js
+++ src/app/MyPage/myPageDao.js
@@ -1,3 +1,5 @@
+const {CATEGORY} = require('../../../modules/constants');
+
 //작성한 코멘트
 async function getWrittenComment(connection, userIdx, startItemIdx, pageItemCnt){
   const query = `
@@ -163,90 +165,93 @@ async function getUserWrittenWith(connection, userIdx, artistIdx, startItemIdx, 
 //댓글 단 글 조회
 async function getUserWrittenWithComment(connection, userIdx, startItemIdx, itemPerPage){
   const query = `
-  SELECT SC.storyIdx                                                                           as idx,
-        1                                                                                     as categoryIdx,
-        (SELECT imageUrl
-          FROM StoryImage SI
-          WHERE SI.storyIdx = S.storyIdx && SI.isDeleted = 'N'
-          LIMIT 1)                                                                             as mainImageUrl,
-        S.title,
-        S.content,
-        U.nickname                                                                            as name,
-        DATE_FORMAT(S.createdAt, '%Y.%m.%d')                                                  as createdAt,
-        SC.originalCreatedAt,
-        (SELECT COUNT(*)
-          FROM StoryLike SL
-          WHERE SL.storyIdx = S.storyIdx)                                                      as totalLikeCnt,
-        IF(EXISTS(SELECT *
-                  FROM StoryLike SL2
-                  WHERE SL2.userIdx = ${userIdx} && SL2.storyIdx = S.storyIdx), 'Y', 'N')     as isLike,
-        (SELECT COUNT(*)
-          FROM StoryComment SC2
-          WHERE SC2.userIdx = ${userIdx} && SC2.storyIdx = SC.storyIdx && SC2.isDeleted = 'N') as totalCommentCnt
+  SELECT *
   FROM (
-          SELECT storyCommentIdx, storyIdx, createdAt as originalCreatedAt
-          FROM StoryComment
-          WHERE userIdx = ${userIdx} && isDeleted = 'N'
-          ORDER BY originalCreatedAt DESC
-          LIMIT 18446744073709551615
-      ) SC
-          JOIN Story S ON S.storyIdx = SC.storyIdx && S.isDeleted = 'N'
-          JOIN User U ON U.userIdx = S.userIdx && U.isDeleted = 'N'
-  GROUP BY SC.storyIdx
-  UNION
-  SELECT QC.qnaIdx                                                                      as idx,
-        2                                                                              as categoryIdx,
-        NULL                                                                           as imageUrl,
-        Q.title,
-        Q.content,
-        U.nickname                                                                     as name,
-        DATE_FORMAT(Q.createdAt, '%Y.%m.%d')                                           as createdAt,
-        QC.originalCreatedAt,
-        (SELECT COUNT(*)
-          FROM QnALike QL
-          WHERE QL.qnaIdx = QC.qnaIdx)                                                  as totalLikeCnt,
-        IF(EXISTS(SELECT *
-                  FROM QnALike QL2
-                  WHERE QL2.userIdx = ${userIdx} && QL2.qnaIdx = QC.qnaIdx), 'Y', 'N') as isLike,
-        (SELECT COUNT(*)
-          FROM QnAComment QC2
-          WHERE QC2.qnaIdx = QC.qnaIdx && QC2.isDeleted = 'N')                          as totalCommentCnt
-  FROM (
-          SELECT qnaIdx, createdAt as originalCreatedAt
-          FROM QnAComment
-          WHERE isDeleted = 'N' && userIdx = ${userIdx}
-          ORDER BY originalCreatedAt DESC
-          LIMIT 18446744073709551615
-      ) QC
-          JOIN QnA Q ON Q.qnaIdx = QC.qnaIdx && Q.isDeleted = 'N'
-          JOIN User U ON U.userIdx = Q.userIdx && U.isDeleted = 'N'
-  GROUP BY QC.qnaIdx
-  UNION
-  SELECT ABC.abTestIdx                                                  as idx,
-        3                                                              as categoryIdx,
-        NULL                                                           as imageUrl,
-        NULL                                                           as title,
-        AB.content,
-        U.nickname                                                     as name,
-        DATE_FORMAT(AB.createdAt, '%Y.%m.%d')                          as createdAt,
-        ABC.originalCreatedAt,
-        NULL                                                           as totalLikeCnt,
-        NULL                                                           as isLike,
-        (SELECT COUNT(*)
-          FROM ABTestComment ABC2
-          WHERE ABC2.abTestIdx = ABC.abTestIdx && ABC2.isDeleted = 'N') as totalCommentCnt
-  FROM (
-          SELECT abTestCommentIdx, abTestIdx, createdAt as originalCreatedAt
-          FROM ABTestComment
-          WHERE userIdx = ${userIdx} && isDeleted = 'N'
-          ORDER BY originalCreatedAt DESC
-          LIMIT 18446744073709551615
-      ) ABC
-          JOIN ABTest AB ON AB.abTestIdx = ABC.abTestIdx && AB.isDeleted = 'N'
-          JOIN Artist A ON A.artistIdx = AB.artistIdx && A.isDeleted = 'N'
-          JOIN User U ON U.userIdx = A.userIdx && U.isDeleted = 'N'
-  GROUP BY ABC.abTestIdx
-  ORDER BY originalCreatedAt DESC
+          SELECT SC.storyIdx                                                                       as idx,
+                  ${CATEGORY.STORY}                                                                 as categoryIdx,
+                  (SELECT imageUrl
+                  FROM StoryImage SI
+                  WHERE SI.storyIdx = S.storyIdx
+                  LIMIT 1)                                                                         as mainImageUrl,
+                  S.title,
+                  S.content,
+                  U.nickname                                                                        as name,
+                  DATE_FORMAT(S.createdAt, '%Y.%m.%d. %H:%i')                                       as createdAt,
+                  SC.originalCreatedAt,
+                  (SELECT COUNT(*)
+                  FROM StoryLike SL
+                  WHERE SL.storyIdx = S.storyIdx)                                                  as totalLikeCnt,
+                  IF(EXISTS(SELECT *
+                            FROM StoryLike SL
+                            WHERE SL.userIdx = ${userIdx} && SL.storyIdx = S.storyIdx), 'Y', 'N')   as isLike,
+                  (SELECT COUNT(*)
+                  FROM StoryComment SC
+                  WHERE SC.userIdx = ${userIdx} && SC.storyIdx = S.storyIdx && SC.isDeleted = 'N') as totalCommentCnt
+          FROM (
+                    SELECT storyIdx, createdAt as originalCreatedAt
+                    FROM StoryComment
+                    WHERE userIdx = ${userIdx} && isDeleted = 'N'
+                    ORDER BY originalCreatedAt DESC
+                    LIMIT 18446744073709551615
+                ) SC
+                    JOIN Story S ON S.storyIdx = SC.storyIdx && S.isDeleted = 'N'
+                    JOIN User U ON U.userIdx = S.userIdx && U.isDeleted = 'N'
+          GROUP BY SC.storyIdx
+          UNION ALL
+          SELECT QC.qnaIdx                                                                    as idx,
+                  ${CATEGORY.QNA}                                                              as categoryIdx,
+                  NULL                                                                         as mainImageUrl,
+                  Q.title,
+                  Q.content,
+                  U.nickname                                                                   as name,
+                  DATE_FORMAT(Q.createdAt, '%Y.%m.%d. %H:%i')                                  as createdAt,
+                  QC.originalCreatedAt,
+                  (SELECT COUNT(*)
+                  FROM QnALike QL
+                  WHERE QL.qnaIdx = QC.qnaIdx)                                                as totalLikeCnt,
+                  IF(EXISTS(SELECT *
+                            FROM QnALike QL
+                            WHERE QL.userIdx = ${userIdx} && QL.qnaIdx = QC.qnaIdx), 'Y', 'N') as isLike,
+                  (SELECT COUNT(*)
+                  FROM QnAComment QC2
+                  WHERE QC2.qnaIdx = QC.qnaIdx && QC2.isDeleted = 'N')                        as totalCommentCnt
+          FROM (
+                    SELECT qnaIdx, createdAt as originalCreatedAt
+                    FROM QnAComment
+                    WHERE isDeleted = 'N' && userIdx = ${userIdx}
+                    ORDER BY originalCreatedAt DESC
+                    LIMIT 18446744073709551615
+                ) QC
+                    JOIN QnA Q ON Q.qnaIdx = QC.qnaIdx && Q.isDeleted = 'N'
+                    JOIN User U ON U.userIdx = Q.userIdx && U.isDeleted = 'N'
+          GROUP BY QC.qnaIdx
+          UNION ALL
+          SELECT ABC.abTestIdx                                                  as idx,
+                  ${CATEGORY.ABTEST}                                             as categoryIdx,
+                  NULL                                                           as mainImageUrl,
+                  NULL                                                           as title,
+                  AB.content,
+                  U.nickname                                                     as name,
+                  DATE_FORMAT(AB.createdAt, '%Y.%m.%d. %H:%i')                   as createdAt,
+                  ABC.originalCreatedAt,
+                  NULL                                                           as totalLikeCnt,
+                  NULL                                                           as isLike,
+                  (SELECT COUNT(*)
+                  FROM ABTestComment ABC2
+                  WHERE ABC2.abTestIdx = ABC.abTestIdx && ABC2.isDeleted = 'N') as totalCommentCnt
+          FROM (
+                    SELECT abTestIdx, createdAt as originalCreatedAt
+                    FROM ABTestComment
+                    WHERE userIdx = ${userIdx} && isDeleted = 'N'
+                    ORDER BY originalCreatedAt DESC
+                    LIMIT 18446744073709551615
+                ) ABC
+                    JOIN ABTest AB ON AB.abTestIdx = ABC.abTestIdx && AB.isDeleted = 'N'
+                    JOIN Artist A ON A.artistIdx = AB.artistIdx && A.isDeleted = 'N'
+                    JOIN User U ON U.userIdx = A.userIdx && U.isDeleted = 'N'
+          GROUP BY ABC.abTestIdx
+      ) R
+  ORDER BY R.originalCreatedAt DESC
   LIMIT ${startItemIdx}, ${itemPerPage};
   `;
   const [rows] = await connection.query(query);
